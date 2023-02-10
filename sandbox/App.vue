48c1fddcda171f5bb47799ab4346739c2fb40e18
pezexd <pedro9031@gmail.com>
2023-02-10 18:09:24 -0400

    Added input component

--- sandbox/App.vue
+++ sandbox/App.vue
@@ -1,11 +1,21 @@
+<script setup lang="ts">
+import Input from '../src/Input.vue'
+</script>
+
 <template>
     <main>
         <h1>Welcome to your your sandbox environment</h1>
+
+        <Input label="firstname" :input-attributes="{
+            placeholder: 'Enter your firstname...',
+            type: 'text',
+            autocomplete: 'given-name',
+        }">
+        First-name
+        </Input>
     </main>
 </template>
-  
-<script setup lang="ts"></script>
-  
+
 <style lang="scss" scoped>
 
 </style>
