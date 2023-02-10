5a7f302bc6742b150bf973d78655a9323079ec87
pezexd <pedro9031@gmail.com>
2023-02-10 17:36:35 -0400

    First release

---
+++ sandbox/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <main>
+        <h1>Welcome to your your sandbox environment</h1>
+    </main>
+</template>
+  
+<script setup lang="ts"></script>
+  
+<style lang="scss" scoped>
+
+</style>
